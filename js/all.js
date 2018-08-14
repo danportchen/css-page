77c512b33b7be1c56b3e792ab82cf87522ca0652
danportchen <danport.chen@gmail.com>
2018-08-14 18:47:30 +0800

    Update 2018-08-14T10:47:30.404Z

---
+++ js/all.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var newFn2 = function newFn2() {
+  console.log('a');
+};
+
+newFn2();
+'use strict';
+
+var newFn1 = function newFn1() {
+  console.log('abcdf');
+};
+
+newFn1();
+//# sourceMappingURL=all.js.map
